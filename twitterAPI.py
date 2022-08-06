dc79b18e5e1149cf9598abf08c26f3f5252b5c88
hino <65232498+Cyndaquil1999@users.noreply.github.com>
2022-08-06 15:47:44 +0900

    Update twitterAPI.py

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,14 +1,48 @@
 import requests, os, json
 
 #Bearer Token
-#何故かos.environにない
 bearer_token = "<BEARER_TOKEN>"
+#何故かos.environにない
 #bearer_token = os.environ.get("BEARER_TOKEN")
 
 
+class collect_userid:
+     
+    def create_url(user):
+        usernames = f"usernames={user}"
+        url = f"https://api.twitter.com/2/users/by?{usernames}"
+        return url
+    
+    
+    def bearer_oauth(r):
+        r.headers["Authorization"] = f"Bearer {bearer_token}"
+        r.headers["User-Agent"] = "v2UserLookupPython"
+        return r
+
+
+    def connect_to_endpoint(url):
+        response = requests.request("GET", url, auth=bearer_oauth,)
+        #print(response.status_code)
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(
+                    response.status_code, response.text
+                )
+            )
+        return response.json()
+
+
+    def main():
+        username = input("TwitterのIDを入力してください: ")
+        url = collect_userid.create_url(username)
+        json_response = connect_to_endpoint(url)
+        return json_response["data"][0]["id"]
+
+
 def make_url():
+    userid = collect_userid
     username = "usernames=<username>"
-    user_id = "<user_id>"
+    user_id = userid.main()
     #user_fields = "user_fields="
     #url = "https://api.twitter.com/2/users/by?{}".format(username)
     url = "https://api.twitter.com/2/users/{}/tweets".format(user_id)
